cf2831c6beefc841d15331e4e0ec9b9ed787efbc
Thierry-Heinz <studioklatz@gmail.com>
2023-04-30 11:12:38 +0200

    initial commit

---
+++ src/examples/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import TableGenerator from "../lib/components/TableGenerator/TableGenerator";
+
+const App = () => (
+  <div style={{ width: 640, margin: "15px auto" }}>
+    <h1>Hello React</h1>
+	<TableGenerator />
+  </div>
+);
+
+export default App;
---
+++ src/lib/components/TableGenerator/TableGenerator.jsx
@@ -0,0 +1,17 @@
+const TableGenerator = () => {
+  return (
+	<table>
+		<thead><th>
+			TableGenerator</th></thead>
+			<tbody>
+				<tr>
+					<th>
+						test firstline
+					</th>
+				</tr>
+			</tbody>
+		
+		</table>
+  )
+}
+export default TableGenerator
